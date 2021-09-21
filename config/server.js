fb6f135d6e7dbf5c98426ae65d0482d7e850d8d5
Rodolfo Hernandez <rodolforippln@gmail.com>
2021-09-20 22:23:05 -0300

    Primeiro commit da API GraphQL com Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3bc95ec68f1a899ff458984cd855e840'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0736ac60-1310-43b3-aee7-8c96040bc403'
+};
